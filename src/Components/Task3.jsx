418cbaf56de52df0793b9559f4ae22b8243588ce
sestradagarcia <stephanie.estradagarcia@sky.uk>
2023-09-26 14:52:40 +0100

    second commit

--- src/Components/Task3.jsx
+++ src/Components/Task3.jsx
@@ -4,29 +4,43 @@ import "./Task.css"
 function Task3() {
     const [search, setsearch] = useState("")
 
-    const data= [{name:"shoe", price: "£2", category: "clothing"}, {name:"pen", price:"£2", category:"stationery"}, {name:"pencil", price:"£2", category:"stationery"}]
-    
+    const data= [{name:"shoe", price: "£2", category: "Clothing"}, {name:"purse", price: "£2", category: "Clothing"}, {name:"pen", price:"£2", category:"Stationery"}, {name:"pencil", price:"£2", category:"Stationery"}]
+
+    const filteredData = search === "" ? data : data.filter(product=> product.name.includes(search));
+
+    const groupByCategory = {};
+
+    for(let p of filteredData){
+        if (!groupByCategory[p.category]){
+            groupByCategory[p.category]= []
+        }
+        groupByCategory[p.category].push(p)
+    }
+
+    const categories = Object.keys(groupByCategory)
+
   return (
     <div>
         <h1>Task 3:</h1>
         <input type="text" placeholder='search...' onChange={(e)=> setsearch(e.target.value)} value={search}/>
         <br />
-        {(search === "" ? data : data.filter(product=> product.name.includes(search))).map(filteredProduct=>( 
-        <table>
+        {categories.map(category=>( 
+        <table key={category}>
             <thead>
                 <tr>
-                    <th>{filteredProduct.category}</th>
+                    <th>{category}</th>
                 </tr>
-            </thead>
-            <tbody>
                 <tr>
                     <th>Name</th>
                     <th>Price</th>
                 </tr>
-                <tr>
-                    <td>{filteredProduct.name}</td>
-                    <td>{filteredProduct.price}</td>
-                </tr>
+            </thead>
+            <tbody>
+                {groupByCategory[category].map(product=>(
+                <tr key = {product.name}>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                </tr>))}
             </tbody>
         </table>))}
     </div>
