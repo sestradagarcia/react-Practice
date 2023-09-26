af2d8070c369bc78586e702a0645d2e66c3271ea
sestradagarcia <stephanie.estradagarcia@sky.uk>
2023-09-26 17:29:31 +0100

    3rd commit

--- src/Components/Task3.jsx
+++ src/Components/Task3.jsx
@@ -1,10 +1,52 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
+import axios from "axios";
 import "./Task.css"
+
+function SearchBar({value, onChange }){
+    return(
+        <input type="text" placeholder='search...' onChange={onChange} value={value}/>
+    )
+}
+
+function ProductTable({category, products}){
+    return(
+        <table key={category}>
+        <thead>
+            <tr>
+                <th>{category}</th>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            {products.map(product=>(
+            <tr key = {product.name}>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+            </tr>))}
+        </tbody>
+    </table>
+
+    )
+}
 
 function Task3() {
     const [search, setsearch] = useState("")
+    const [data, setData] = useState([])
 
-    const data= [{name:"shoe", price: "£2", category: "Clothing"}, {name:"purse", price: "£2", category: "Clothing"}, {name:"pen", price:"£2", category:"Stationery"}, {name:"pencil", price:"£2", category:"Stationery"}]
+        useEffect(function () {
+        axios.get("http://localhost:4494/products")
+            .then(function (response) {
+                // access the response body with .data
+                console.log("RESPONSE RECEIVED");
+                setData(response.data);
+            })
+            .catch(err => console.error(err));
+    }, []); // use effect -> runs ONLY when component first loads
+
+    
 
     const filteredData = search === "" ? data : data.filter(product=> product.name.includes(search));
 
@@ -22,27 +64,10 @@ function Task3() {
   return (
     <div>
         <h1>Task 3:</h1>
-        <input type="text" placeholder='search...' onChange={(e)=> setsearch(e.target.value)} value={search}/>
         <br />
-        {categories.map(category=>( 
-        <table key={category}>
-            <thead>
-                <tr>
-                    <th>{category}</th>
-                </tr>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {groupByCategory[category].map(product=>(
-                <tr key = {product.name}>
-                    <td>{product.name}</td>
-                    <td>{product.price}</td>
-                </tr>))}
-            </tbody>
-        </table>))}
+        <SearchBar value = {search} onChange = {(e) => setsearch(e.target.value)} />
+        {categories.map(category=>( <ProductTable key={category} category= {category} products={groupByCategory[category]}/>
+       ))}
     </div>
   )
 }
